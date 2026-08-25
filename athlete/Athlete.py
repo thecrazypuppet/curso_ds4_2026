52a5b26ddee6ae8982d8eedaec298511cb74a1c7
Francisco <jastorgagfra@gmail.com>
2026-08-24 17:43:17 -0700

    Actividades
    modified file

--- athlete/Athlete.py
+++ athlete/Athlete.py
@@ -1,24 +1,31 @@
 class Athlete:
     """ A class to represent an athlete.
     """
-    def __init__(self, name:
-         self.sport)
-
-    def __str__()
-        return f"Athlete name:{self}"
+    def __init__(self, name: str, age: int, sport: str):
+        self.name = name
+        self.age = age
+        self.sport = sport
+    def __str__(self):
+        return f"Athlete name:{self.name}, age:{self.age}"
+    def __repr__(self):
+        return f"Athelete(name='{self.name}', age={self.age}, sport='{self.sport}')"
     def display(self):
-        print(f"|{self.name}|{self.age}|{self.sport}|")
+            print(f"|{self.name}|{self.age}|{self.sport}|")
 
-    def main():
+def main():
         """ Test the class"""
         a = Athlete("Ana G", 25, "200m")
         b = Athlete("usain bolt", 30,"100m")
+        print(a)
+        print(b)
         a.display()
         b.display()
         print(repr(a))
-        #c = eval(repr(b))
-        #print(c)
-        #print(f"c is b:{c is b}")
+        print(repr(b))
+        c = eval(repr(b))
+        print(c)
+        print(f"c is b:{c is b}")
+        print(f"id c:{id(c)}, id b: {id(b)}")
 
 if __name__ == "__main__":
     main()
